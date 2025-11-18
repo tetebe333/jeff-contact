b81664850533d8a8afe3c205a30aab0aa2ace251
Tetebe Jeffrey <tetebejeffrey@gmail.com>
2025-11-18 16:50:08 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { localStorageStore } from "@skeletonlabs/skeleton";
+	import { writable, type Writable } from 'svelte/store';
+	
+	let inputName = "";
+	let inputNumber ="";
+
+	//writable type
+	interface contact{
+		name: string
+		phoneNumber:string
+	}
+
+    //creating localStorageStore
+	const contactStore: Writable<contact[]> = localStorageStore("contactStore", [])
+
+	//add contact function
+	function addContact() {
+		$contactStore = [
+			{
+				name: inputName,
+				phoneNumber: inputNumber
+			},
+			...$contactStore
+		]
+	}
+
+	//delete contact function
+	function deleteContact(index=-1){
+		if ( window.confirm("are our sure you want to delete " + $contactStore[index].name)){
+			 $contactStore = $contactStore.filter((contact, contactIndex) => contactIndex !==index);
+		}
+	}
+</script>
+
+<main class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">contactList</h1>
+		<p>All your contacts in one persistent state!</p>
+
+		<label class="label">
+			<span>Name</span>
+			<input bind:value={inputName} type="text" class="input" placeholder="Name">
+		</label>
+
+		<label class="label">
+			<span>Phone Number</span>
+			<input bind:value={inputNumber} type="text" class="input" placeholder="Phone Number">
+		</label>
+
+		<button on:click={addContact} type="button" class="btn variant-filled">Add contact</button>
+		<hr />
+		
+		<h2 class="h2">Your contacts</h2>
+		{#each $contactStore as contact, index}
+			<div class="card p-2">
+				<h1>{contact.name}</h1>
+				<h1>{contact.phoneNumber}</h1>
+				<button on:click={() =>deleteContact(index)} type="button" class="btn btn-sm variant-filled-error">Delete</button>
+			</div>
+		{/each}
+	</div>
+</main>
